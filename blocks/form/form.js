aec94b3f023d2bb8cee190fc635a4180c7669b8e
Dmytro Omelchuk <omeldm@gmail.com>
2016-06-03 16:11:07 +0300

    Added order information block

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,13 +1,13 @@
-function showID(anID) {
-	var allForms = document.getElementsByClassName("form");
+function showIDWithParent(anID, parentClass) {
+	var allForms = document.getElementsByClassName("form_display");
 	var formToShow = document.getElementById(anID);
 
 	for (var index = 0; index < allForms.length; index++) {
 		if (allForms[index] === formToShow) {
-			formToShow.classList.remove("form_hidden");
+			formToShow.classList.remove("form_display_hidden");
 			var obj = this;
 			continue;
 		}
-		allForms[index].classList.add('form_hidden');
+		allForms[index].classList.add('form_display_hidden');
 	}
 }
