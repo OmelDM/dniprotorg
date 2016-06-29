b047ff241a442f6abfb205f29b3411575be0c872
Dmytro Omelchuk <omeldm@gmail.com>
2016-06-29 18:46:29 +0300

    Reworked cart to be used with shopkeeper

--- blocks/modal/modal.js
+++ blocks/modal/modal.js
@@ -1,13 +1,13 @@
 function openModalWithID(anID) {
 	"use strict";
 
-	var modalBlock = document.getElementById(anID);
-	modalBlock.classList.add("modal-container_display");
+	var modalKeeperBlock = document.getElementById(anID);
+	modalKeeperBlock.classList.add("modal-keeper_display");
 }
 function closeModalWithID(anID) {
 	"use strict";
 
-	var modalBlock = document.getElementById(anID);
-	modalBlock.classList.remove("modal-container_display");
+	var modalKeeperBlock = document.getElementById(anID);
+	modalKeeperBlock.classList.remove("modal-keeper_display");
 
 }
